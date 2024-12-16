ff3938b4a2dc8d1d73046a0a13ef7e83a17c20c1
Shua0x0 <Shua0x0@gmail.com>
2024-12-16 21:28:48 +0100

    LevelDesign0.2

--- castle-cimblers/Scripts/player.gd
+++ castle-cimblers/Scripts/player.gd
@@ -1,19 +1,18 @@
 extends CharacterBody2D
 
 #Player movement variables
-@export var SPEED = 100
+@export var SPEED = 200
 @export var GRAVITY = 200
 @export var jump_height = -100
-
-var is_attacking = false
-var is_climbing = false
 
 
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 	horizontal_movement()
 	move_and_slide()
-	player_animations()
+	
+	if !Global.is_attacking || !Global.is_climbing:
+		player_animations()
 
 func horizontal_movement():
 	var horizontal_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -36,25 +35,28 @@ func player_animations():
 		
 func _input(event):
 	if event.is_action_pressed("ui_attack"):
-		is_attacking = true
+		Global.is_attacking = true
 		$AnimatedSprite2D.play("attack")
 		
 	if event.is_action_pressed("ui_jump") and is_on_floor():
 		velocity.y = jump_height
 		$AnimatedSprite2D.play("jump")
 		
-	if is_climbing == true:
-		if event.is_action_pressed("ui_up") and is_on_floor():
+		if Global.is_climbing == true:
+			if !Input.is_anything_pressed():
+				$AnimatedSprite2D.play("idle")
+				
+	if Global.is_climbing == true:
+		if event.is_action_pressed("ui_up"):
 			$AnimatedSprite2D.play("climb")
 			GRAVITY = 100
-			velocity.y = -200
+			velocity.y = -180
 			
 	else:
 			GRAVITY = 200
-			is_climbing = false
+			Global.is_climbing = false
 		
-		
-
-
+	
 func _on_animated_sprite_2d_animation_finished() -> void:
-	is_attacking = false
+	Global.is_attacking = false
+	Global.is_climbing = false
